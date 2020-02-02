102f5f518f9b9a05d88a97b129e89496f992de24
guestt <guestt@MacBook-Pro-Dmitrij.local>
2020-02-02 13:10:19 +0300

    Префаб дырки - вода течет в нужную комнату - надо добавить частицы - надо добавить механизм затыкания

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -4,49 +4,35 @@ using UnityEngine;
 
 public class Hole : MonoBehaviour
 {
-    public float totalArea;
+    public Room room;
 
-    PolygonCollider2D col;
+    public float rate = 0.5f;
 
-    void Awake() {
-        col = gameObject.GetComponent<PolygonCollider2D>();
-        Mesh mesh = null;
-        totalArea = SuperficieIrregularPolygon(col);
-        Debug.Log(string.Format("GO: {0}; Collider area: {1}", gameObject.name, totalArea));
+    public bool isClosed = false;
+
+    void Awake()
+    {
+        room = GetRoom();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void ModifyCollider() {
-        /*
-            Run this when duct tape is placed AND if it intersects with this collider.
-            On this step, we should somehow modify mesh collider in place
-            Then we'll check if emitter transform is still within the newly generated collider.
-            If it's not, turn off water emission.
-            ???
-            PROFIT
-        */
-    }
-
-    float SuperficieIrregularPolygon(PolygonCollider2D col) {
-        List<Vector2> list = new List<Vector2>(col.points);
-        float temp = 0;
-        for (int i=0; i < list.Count ; i++) {
-            if (i != list.Count - 1) {
-                float mulA = list[i].x * list[i+1].y;
-                float mulB = list[i+1].x * list[i].y;
-                temp = temp + ( mulA - mulB );
-            } else {
-                float mulA = list[i].x * list[0].y;
-                float mulB = list[0].x * list[i].y;
-                temp = temp + ( mulA - mulB );
-            }
+        if (room && !isClosed) {
+            room.UpdateWater(rate * Time.deltaTime * 3);
         }
-        temp *= 0.5f;
-        return Mathf.Abs(temp);
+    }
+
+    private Room GetRoom()
+    {
+        BoxCollider2D col2d = gameObject.GetComponent<BoxCollider2D>();
+        Collider2D[] rooms = new Collider2D[1];
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.SetLayerMask(LayerMask.GetMask("RoomLogic"));
+        col2d.OverlapCollider(contactFilter, rooms);
+
+        Collider2D roomCollider = rooms[0];
+
+        return roomCollider.gameObject.GetComponent<Room>();
     }
 }
